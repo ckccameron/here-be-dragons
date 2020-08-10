598a9e11a257843c8379342b7c2707e5827b024a
Cam C <ckc.cameron@gmail.com>
2020-08-10 12:31:22 -0600

    Pass test for getting subset of politics stories

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -18,13 +18,18 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_array_of_stories
     result = @hash[:results]
-    
+
     assert result.is_a? (Array)
     assert_equal 44, result.count
   end
 
   def test_it_can_get_all_stories_with_subsection_of_politics
-    #Set your code to the local variable, "result"
+    result = []
+    @hash[:results].each do |story|
+      if story[:subsection] == "Politics"
+        result << story
+      end
+    end
 
     assert result.is_a? (Array)
     assert_equal 6, result.count
@@ -33,5 +38,4 @@ class NytimesTest < Minitest::Test
     assert_equal "Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.",
       result.last[:title]
   end
-
 end
